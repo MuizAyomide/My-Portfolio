0babf1fa11fb059649c3c0fbe3e41e7f0d81860c
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-28 01:33:38 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 const App = () => {
   return (
     <div>
-      Hello
+     <Navbar/>
     </div>
   )
 }
