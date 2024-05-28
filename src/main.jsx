ea4268b7846a0345d651e1bd97a4d565f383a6dd
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-28 01:33:50 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import '../src/Styles/Modern-Normalize.css'
+import '../src/Styles/Utils.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
