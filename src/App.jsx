a71e766345f15ba97044e6a0de5fcca2cf8f8de1
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-29 22:45:44 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+import About from './Components/About/About'
+import Featured from './Components/Featured/Featured'
+import Project from './Components/Projects/Project'
+import Contact from './Components/Contact/Contact'
 
 const App = () => {
   return (
     <div>
      <Navbar/>
+     <Hero/>
+     <About/>
+     <Featured/>
+     <Project/>
+     <Contact/>
     </div>
   )
 }
