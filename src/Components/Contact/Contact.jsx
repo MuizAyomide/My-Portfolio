5b1b7ceb1d8d6a471ba8996e3913db04141f06e4
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-29 22:46:12 +0100

    commit

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <div className='section contact container'>
+      <h2 className='contact-title'>Get In Conatct</h2>
+      <p className='contact-desc'>
+        Whenever you are starting a project, have business
+         inquires or just want to say hi, my inbox is always open
+          so feel free to reach out and I will get back to you as soon as possible.
+      </p>
+      <a href='mailto:akinrinolamuiz@gmail.com'
+      target='_blank'
+      className='btn contact-btn'>Reach Out</a>
+    </div>
+  )
+}
+
+export default Contact
