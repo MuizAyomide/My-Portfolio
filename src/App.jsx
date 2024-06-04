741f67c26d0ac9c7469233245111793e55207904
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-06-04 23:56:20 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import './App.css'
-import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
-import About from './Components/About/About'
-import Featured from './Components/Featured/Featured'
-import Project from './Components/Projects/Project'
-import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer'
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import About from "./Components/About/About";
+import Featured from "./Components/Featured/Featured";
+import Project from "./Components/Projects/Project";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
     <div>
-     <Navbar/>
-     <Hero/>
-     <About/>
-     <Featured/>
-     <Project/>
-     <Contact/>
-     <Footer/>
+      <Navbar />
+      <Hero />
+      <About />
+      <Featured />
+      <Project />
+      <Contact />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
