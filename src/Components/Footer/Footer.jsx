9c7e42036ac491fd1b3c5e334619a9f340c1adff
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-06-04 23:57:20 +0100

    commit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className='footer container section'>
-      <h3 className='footer-title'>~ Akinrinola Muiz Ayomide ~</h3>
+    <div className="footer container section">
+      <h3 className="footer-title">~ Akinrinola Muiz Ayomide ~</h3>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
