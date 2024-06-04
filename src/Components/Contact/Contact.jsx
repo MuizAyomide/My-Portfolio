61f3f74e1a366b193ed1d081e4a38376a720cda5
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-06-04 23:57:03 +0100

    commit

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import './Contact.css'
+import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div className='section contact container'>
-      <h2 className='contact-title'>Get In Contact</h2>
-      <p className='contact-desc'>
-        Whenever you are starting a project, have business
-         inquires or just want to say hi, my inbox is always open
-          so feel free to reach out and I will get back to you as soon as possible.
+    <div className="section contact container">
+      <h2 className="contact-title">Get In Contact</h2>
+      <p className="contact-desc">
+        Whenever you are starting a project, have business inquires or just want
+        to say hi, my inbox is always open so feel free to reach out and I will
+        get back to you as soon as possible.
       </p>
-      <a href='mailto:akinrinolamuiz@gmail.com'
-      target='_blank'
-      className='btn contact-btn'>Reach Out</a>
+      <a
+        href="mailto:akinrinolamuiz@gmail.com"
+        target="_blank"
+        className="btn contact-btn"
+      >
+        Reach Out
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
