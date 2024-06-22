95dafd97899d0d486badcfebce661bb66605fe06
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-06-22 23:20:43 +0100

    commit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer container section">
+      
       <h3 className="footer-title">~ Akinrinola Muiz Ayomide ~</h3>
     </div>
   );
