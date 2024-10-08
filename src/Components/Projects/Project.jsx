d5090e5037b41930966078dcddfb80515b950b4d
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-10-08 01:58:07 +0100

    commit

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -13,28 +13,40 @@ const Project = () => {
       </p>
       <div className="project-container">
         {/* project 1 */}
-        <h3 className="project-project-title">Admin Dashboard</h3>
+        <h3 className="project-project-title">Full Stack E-Commerce Website</h3>
         <div className="project-image-wrapper project-image1">
-          <img className="project-image" src="/admin-dashboard.png" alt="" />
+          <img className="project-image" src="/forever.JPG" alt="" />
         </div>
         <div className="project-project project-content1">
-          <h3 className="project-project-sub">Admin Dashboard</h3>
+          <h3 className="project-project-sub">Full Stack E-Commerce Website</h3>
           <p className="project-project-desc1">
-            Designed and developed a robust admin dashboard that streamlines
-            data visualization, user management, and reporting capabilities for
-            seamless business operations.
+            The Full Stack E-Commerce Website is a comprehensive online shopping
+            platform that allows users to browse, search, and purchase a wide
+            range of clothing products. The project leverages modern web
+            technologies to provide a responsive and user-friendly interface,
+            ensuring a seamless shopping experience. It includes features like
+            user authentication, product management, shopping cart
+            functionality, payment integration, and an admin dashboard for
+            managing orders and products.
           </p>
           <p className="project-project-desc2">
-            
             This admin dashboard project showcases my expertise in modern
-            frontend technologies, including React, TypeScript, SCSS, and Vite.
-            The result is a highly performant and scalable web application.
+            frontend technologies, including React, Tailwind CSS, Context API,
+            and Vite. and backend technologies like Node.js, Express.js, Mongo
+            DB, Razor pay and Stripe. The result is a highly performant and
+            scalable web application.
           </p>
-          <Link 
-          onClick={()=>{
-            window.open('https://educity-three.vercel.app/', '_blank')
-          }}
-          className="project-btn btn">Website</Link>
+          <Link
+            onClick={() => {
+              window.open(
+                "https://full-stack-eccomerce-app.vercel.app/",
+                "_blank"
+              );
+            }}
+            className="project-btn btn"
+          >
+            Website
+          </Link>
         </div>
 
         {/* project 2 */}
@@ -56,12 +68,14 @@ const Project = () => {
             built a clean, modern, and highly interactive platform to showcase
             the institution's programs and resources.
           </p>
-          <Link 
-          onClick={()=>{
-            window.open('https://educity-university.vercel.app/', '_blank')
-          }}
-        
-          className="project-btn btn">Website</Link>
+          <Link
+            onClick={() => {
+              window.open("https://educity-university.vercel.app/", "_blank");
+            }}
+            className="project-btn btn"
+          >
+            Website
+          </Link>
         </div>
 
         {/* project 3 */}
@@ -88,10 +102,16 @@ const Project = () => {
             various devices.
           </p>
           <Link
-          onClick={()=>{
-            window.open('https://ecommerce-frontend-pi-two.vercel.app/', '_blank')
-          }}
-          className="project-btn btn">Website</Link>
+            onClick={() => {
+              window.open(
+                "https://ecommerce-frontend-pi-two.vercel.app/",
+                "_blank"
+              );
+            }}
+            className="project-btn btn"
+          >
+            Website
+          </Link>
         </div>
 
         {/* project 4 */}
@@ -102,22 +122,28 @@ const Project = () => {
         <div className="project-project project-content4">
           <h3 className="project-project-sub">E-commerce Website</h3>
           <p className="project-project-desc1">
-          Developed a clean, modern ecommerce website using a tech stack of
+            Developed a clean, modern ecommerce website using a tech stack of
             React, JavaScript, and Vite. The result is a visually appealing and
             highly interactive platform.
           </p>
           <p className="project-project-desc2">
-            Shoppify  Ecommerce website project demonstrates my expertise in crafting
-            visually appealing, user-friendly online shopping experiences.
-            Leveraging a modern tech stack, I built a responsive, feature-rich
-            platform that streamlines the customer journey from browsing to
-            checkout.
+            Shoppify Ecommerce website project demonstrates my expertise in
+            crafting visually appealing, user-friendly online shopping
+            experiences. Leveraging a modern tech stack, I built a responsive,
+            feature-rich platform that streamlines the customer journey from
+            browsing to checkout.
           </p>
-          <Link 
-          onClick={()=>{
-            window.open('https://ecommerce-frontend-pi-two.vercel.app/', '_blank')
-          }}
-          className="project-btn btn">Website</Link>
+          <Link
+            onClick={() => {
+              window.open(
+                "https://ecommerce-frontend-pi-two.vercel.app/",
+                "_blank"
+              );
+            }}
+            className="project-btn btn"
+          >
+            Website
+          </Link>
         </div>
 
         {/* project 5 */}
@@ -139,19 +165,79 @@ const Project = () => {
             a clean, modern, and highly interactive platform to showcase the
             institution's programs and resources.
           </p>
-          <Link 
-          onClick={()=>{
-            window.open('https://unilag-university-website.vercel.app/', '_blank')
-          }}
-          className="project-btn btn">Website</Link>
+          <Link
+            onClick={() => {
+              window.open(
+                "https://unilag-university-website.vercel.app/",
+                "_blank"
+              );
+            }}
+            className="project-btn btn"
+          >
+            Website
+          </Link>
         </div>
 
         {/* project 6 */}
-        <h3 className="project-project-title">Chat App</h3>
+        <h3 className="project-project-title">YouTube Clone</h3>
         <div className="project-image-wrapper project-image6">
-          <img className="project-image" src="/chat-app.JPG" alt="" />
+          <img className="project-image" src="/vidtube.png" alt="" />
         </div>
         <div className="project-project project-content6">
+          <h3 className="project-project-sub">YouTube Clone</h3>
+          <p className="project-project-desc1">
+          This project focuses on creating an engaging video-sharing platform with an easy-to-use interface.
+          </p>
+          <p className="project-project-desc2">
+            The YouTube Clone is a web application that replicates the
+            functionalities of YouTube. The project focuses on creating an
+            engaging video-sharing platform with a user-friendly interface,
+            real-time interactions, using the YouTube API to handle video uploads
+            and streaming.
+          </p>
+          <Link
+            onClick={() => {
+              window.open(
+                "https://youtube-clone-indol-sigma.vercel.app/",
+                "_blank"
+              );
+            }}
+            className="project-btn btn"
+          >
+            Website
+          </Link>
+        </div>
+
+        {/* project 7 */}
+        <h3 className="project-project-title">Admin Dashboard</h3>
+        <div className="project-image-wrapper project-image7">
+          <img className="project-image" src="/admin-dashboard.png" alt="" />
+        </div>
+        <div className="project-project project-content">
+          <h3 className="project-project-sub">Admin Dashboard</h3>
+          <p className="project-project-desc1">
+            Designed and developed a robust admin dashboard that streamlines
+            data visualization, user management, and reporting capabilities for
+            seamless business operations.
+          </p>
+          <p className="project-project-desc2">
+            
+            This admin dashboard project showcases my expertise in modern
+            frontend technologies, including React, TypeScript, SCSS, and Vite.
+            The result is a highly performant and scalable web application.
+          </p>
+          <Link 
+          onClick={()=>{
+            window.open('https://educity-three.vercel.app/', '_blank')
+          }}
+          className="project-btn btn">Website</Link>
+        </div>
+        {/* project 8 */}
+        <h3 className="project-project-title">Chat App</h3>
+        <div className="project-image-wrapper project-image8">
+          <img className="project-image" src="/chat-app.JPG" alt="" />
+        </div>
+        <div className="project-project project-content8">
           <h3 className="project-project-sub">Chat App</h3>
           <p className="project-project-desc1">
             This chat app project showcases my skills in building real-time,
@@ -163,11 +249,17 @@ const Project = () => {
             Firebase, redux and Vite, I created a responsive and feature-rich
             platform for users to connect and collaborate.
           </p>
-          <Link 
-          onClick={()=>{
-            window.open('https://ecommerce-frontend-pi-two.vercel.app/', '_blank')
-          }}
-          className="project-btn btn">Website</Link>
+          <Link
+            onClick={() => {
+              window.open(
+                "https://ecommerce-frontend-pi-two.vercel.app/",
+                "_blank"
+              );
+            }}
+            className="project-btn btn"
+          >
+            Website
+          </Link>
         </div>
       </div>
     </div>
